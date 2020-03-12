bc1f690c337c7b66edfff9ec6cd59ba760e51454
KarenOn <55865352+KarenOn@users.noreply.github.com>
2020-03-11 20:59:12 -0400

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype_deep
+{
+    class Animal : ICloneable
+    {
+        public int Patas { get; set; }
+        public string Nombre { get; set; }
+
+        public Detalles Rasgos { get; set; }
+
+        public object Clone()
+        {
+            Animal clonado = this.MemberwiseClone() as Animal;
+            Detalles detalles = new Detalles();
+            detalles.Color = this.Rasgos.Color;
+            detalles.Raza = this.Rasgos.Raza;
+            clonado.Rasgos = detalles;
+            return clonado;
+        }
+    }
+
+    public class Detalles
+    {
+        public string Color { get; set; }
+        public string Raza { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Prototype_deep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal am = new Animal() { Nombre = "Mauricio", Patas = 4 };
+            am.Rasgos = new Detalles();
+            am.Rasgos.Color = "Negro";
+            am.Rasgos.Raza = "Rialengo";
+
+            Animal amclon = am.Clone() as Animal;
+            amclon.Rasgos.Color = "Marron";
+            amclon.Nombre = "Papolo";
+
+            Console.WriteLine("Color del animal original: " + am.Rasgos.Color);
+            Console.WriteLine("Color del animal clonado: " + amclon.Rasgos.Color);
+            Console.WriteLine("Nombre del animal original: " + am.Nombre);
+            Console.WriteLine("Nombre del animal clonado: " + amclon.Nombre);
+            Console.ReadKey();
+        }
+    }
+}
